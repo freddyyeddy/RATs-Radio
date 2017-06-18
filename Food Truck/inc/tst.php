7e5d06da43e8b032ec375b058fa1258045b6c793
freddyyeddy <freddyyeddy@users.noreply.github.com>
2017-06-18 17:16:16 -0400

    Automatic Backup @ Sun Jun 18 17:16:15 EDT 2017

--- Food Truck/inc/tst.php
+++ Food Truck/inc/tst.php
@@ -1,5 +1,8 @@
+<head>
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <!-- <script src="js/contact.js"></script> -->
 	<script>
 		function captchaSubmit(data) {
@@ -8,7 +11,7 @@ var email = $("#email").val();
 			var data = $("#data").val();
 var response = grecaptcha.getResponse();
 // Returns successful data submission message when the entered information is stored in database.
-var dataString = 'name='+ name + '&email='+ email + '&response='+ response;
+var dataString = 'name='+ name + '&email='+ email + '$data=' + data + '&response='+ response ;
 if(name==''||email==''||data=='')
 {
 alert("Please Fill All Fields");
@@ -23,20 +26,24 @@ data: dataString,
 cache: false,
 success: function(result){
 alert(result);
+	$("#form")[0].reset();
 }
 });
 }
 return false;
+			
 		}
 	</script>
-<form id='demo-form' action="" method="POST">
+<a href="#popupLogin" data-rel="popup" data-position-to="window" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-a" data-transition="pop">Contact Form</a>
+<div data-role="popup" id="popupLogin" data-theme="a" class="ui-corner-all">
+<form id='form' action="" method="POST">
+	        <div style="padding:10px 20px;">
+            <h3>Contact Us</h3>
 	<label>Name<input id="name" type="text" name="name"></label><br>
 	<label>email<input id="email" type="email" name="last"></label><br>
 	<label>message<input id="data" type="text" name="data"></label><br>
 	<button id="submit" class="g-recaptcha" data-sitekey="6Le5xyUUAAAAAFdnBS-1QU1tEHO5jrsvsLHAVTmu" data-callback="captchaSubmit" data-badge="inline">Submit</button>
-	<p>Please note this form is protected by reCaptcha.</p>
-	<!-- 		<style>
-		.g-recaptcha {display: none;}
-	</style> -->
-	<br/>
+						 </div>
 </form>
+	</div>
+
--- Food Truck/index.php
+++ Food Truck/index.php
@@ -541,52 +541,72 @@ Smoke.
                 </div>
             </div>
             <div class="row">
+							
                 <div class="col-lg-8 col-lg-offset-2">
+
+
+							<div>
 									You Can Email us at <a href="mailto:fns@firensmoketruck.com?Subject=Catering Quesions" target="_blank">fns@firensmoketruck.com</a>
 									or use this form
-									
-                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
-                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-                    <form name="sentMessage" id="contactForm" novalidate>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label for="name">Name</label>
-                                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label for="email">Email Address</label>
-                                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label for="phone">Phone Number</label>
-                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label for="message">Message</label>
-                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <br>
-                        <div id="success"></div>
-                        <div class="row">
-                            <div class="form-group col-xs-12">
-                                <button type="submit" class="btn btn-success btn-lg">Send</button>
-                            </div>
-                        </div>
-                    </form>
+							</div>
+             	<script>
+		function captchaSubmit(data) {
+			var name = $("#name").val();
+var email = $("#email").val();
+			var data = $("#data").val();
+var response = grecaptcha.getResponse();
+// Returns successful data submission message when the entered information is stored in database.
+var dataString = 'name='+ name + '&email='+ email + '$data=' + data + '&response='+ response ;
+if(name==''||email==''||data=='')
+{
+alert("Please Fill All Fields");
+}
+else
+{
+// AJAX Code To Submit Form.
+$.ajax({
+type: "POST",
+url: "contact.php",
+data: dataString,
+cache: false,
+success: function(result){
+alert(result);
+	$("#form")[0].reset();
+}
+});
+}
+return false;
+			
+		}
+	</script>
+<div class="box">
+	<a class="button" href="#contact-form">Contact Form</a>
+</div>
+
+<div id="contact-form" class="overlay">
+	<div class="popup">
+		<h2>Contact Us</h2>
+		<a class="close" href="#">&times;</a>
+		<div class="content">
+			 <form id='form' action="" method="POST">
+	        <div>
+							 <a class="close" href="#">&times;</a>
+	<label>Name<input id="name" type="text" name="name"></label><br>
+	<label>email<input id="email" type="email" name="last"></label><br>
+	<label>message<input id="data" type="text" name="data"></label><br>
+	<button id="submit" class="g-recaptcha" data-sitekey="6Le5xyUUAAAAAFdnBS-1QU1tEHO5jrsvsLHAVTmu" data-callback="captchaSubmit" data-badge="inline">Submit</button>
+						 
+	</div>
+</form>
+		</div>
+	</div>
+</div>
+
+
                 </div>
             </div>
         </div>
+			
     </section>
 
     <!-- Footer -->
